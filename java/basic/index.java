8056e5a2d985bf80186f9d4fe21206c0c79eca1b
omeatai <omeatai@gmail.com>
2025-10-31 16:45:03 -0600

    updates

---
+++ java/basic/index.java
@@ -0,0 +1,9 @@
+class Index{
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+        System.out.println("Hello, World!");
+    }
+}
