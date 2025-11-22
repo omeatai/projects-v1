5bb1a08e00a7a837452fb030423af4f7fff6b689
Ifeanyi Omeata <omeatai@gmail.com>
2025-11-21 23:36:55 -0700

    updates

--- app_js/sample_1/app.js
+++ app_js/sample_1/app.js
@@ -1,22 +1,25 @@
-const h1 = document.getElementById('title');
-h1.style.color = 'red';
+const h1 = document.getElementById("title");
+h1.style.color = "red";
 
-const headings = document.getElementsByTagName('h2');
+const headings = document.getElementsByTagName("h2");
 console.log(headings);
 console.log(headings.length);
-headings[0].style.color = 'blue';
+headings[0].style.color = "blue";
 
-const items = document.getElementsByTagName('li');
-console.log(items);
+const result = document.querySelector("#result");
+result.style.backgroundColor = "black";
+result.style.color = "white";
 
-const listedItems = Array.from(items);
-const listedItems2 = [...items];
-console.log(listedItems);
-console.log(listedItems2);
-
-listedItems[1].style.fontWeight = 'bold';
-listedItems.forEach((item) => {
-    item.style.color = 'green';
-    item.style.fontSize = '20px';
+const students = document.querySelectorAll(".special");
+console.log(students);
+students.forEach((student) => {
+  student.style.color = "pink";
 });
 
+const lastStudent = document.querySelector(".last");
+console.log(lastStudent);
+lastStudent.style.fontSize = "30px";
+
+const lastStudent2 = document.querySelector("li:last-child");
+console.log(lastStudent2);
+lastStudent2.innerHTML += " - color is pink.";
