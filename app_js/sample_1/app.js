f25dc012c2c9cfdfe6ee61bcb640a3f013967909
Ifeanyi Omeata <omeatai@gmail.com>
2025-11-23 20:28:47 -0700

    updates

--- app_js/sample_1/app.js
+++ app_js/sample_1/app.js
@@ -1,26 +1,7 @@
-const first = document.getElementById('first');
-const second = document.getElementById('second');
-const third = document.getElementById('third');
-const forth = document.getElementById('forth');
+// create empty heading element
+const h1 = document.createElement("h1");
 
-const classValue = first.className;
-console.log('Class value of first element:', classValue);
-
-second.className = 'colors2';
-const classValue2 = second.className;
-console.log('Class value of second element:', classValue2);
-
-third.classList.add('colors');
-third.classList.add('text')
-const classValue3 = third.className;
-console.log('Class value of third element:', classValue3);
-
-forth.classList.add('colors2', 'text');
-const classValue4 = forth.className;
-console.log('Class value of forth element:', classValue4);
-forth.classList.remove('colors2');
-const classValue5 = forth.className;
-console.log('Class value of forth element after removing colors2 class:', classValue5);
-
-const hasTextClass = third.classList.contains('text');
-console.log('Does third element have "text" class?', hasTextClass);
+// prepend with InnerText
+h1.innerText = "I'm the created Text element";
+document.body.prepend(h1);
+console.log(document.body.children);
