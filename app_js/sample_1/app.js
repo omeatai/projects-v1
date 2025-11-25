3a8de6da5d82db740b859259d6aa973e645b711b
Ifeanyi Omeata <omeatai@gmail.com>
2025-11-24 18:03:48 -0700

    updates

--- app_js/sample_1/app.js
+++ app_js/sample_1/app.js
@@ -5,3 +5,13 @@ const h1 = document.createElement("h1");
 h1.innerText = "I'm the created Text element";
 document.body.prepend(h1);
 console.log(document.body.children);
+
+// remove
+const header = document.querySelector("#header");
+header.remove();
+
+// removeChild
+const result = document.querySelector("#result");
+const heading = result.querySelector("h1");
+result.removeChild(heading);
+console.log(result.children);
