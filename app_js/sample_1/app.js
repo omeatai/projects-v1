369a32f804cf51af8635a2959e09419fb6daab21
omeatai <omeatai@gmail.com>
2025-11-27 22:09:51 -0700

    updates

--- app_js/sample_1/app.js
+++ app_js/sample_1/app.js
@@ -1,30 +1,15 @@
 
 const btn = document.querySelector('.btn');
-const header = document.querySelector('#header');
-const p = document.querySelector('p');
+const nameInput = document.querySelector('#name');
 
-function clickEvent() {
-   console.log('Button was clicked!');
-}
-
-function mouseDownEvent() {
-    console.log('Mouse is Down');
-}
-
-function mouseUpEvent() {
-    console.log('Mouse is Up');
-}
-
-btn.addEventListener('click', clickEvent);
-btn.addEventListener('mousedown', mouseDownEvent);
-btn.addEventListener('mouseup', mouseUpEvent);
-
-header.addEventListener('mouseenter', function() {
-    console.log('Mouse entered the header');
-    header.classList.add('red');
+nameInput.addEventListener('keypress', function(e) {
+    console.log(`You pressed a key: "${e.key}"`);
 });
 
-header.addEventListener('mouseleave', function() {
-    console.log('Mouse left the header');
-    header.classList.remove('red');
+nameInput.addEventListener('keydown', function(e) {
+    console.log(`Key down: "${e.key}"`);
+});
+
+nameInput.addEventListener('keyup', function(e) {
+    console.log(`Key up: "${e.key}"`);
 });
